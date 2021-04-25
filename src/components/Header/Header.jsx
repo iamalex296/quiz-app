777bb6f89f98792d3f10f9bfa002b93397e05d6e
alex <alexizana10@gmail.com>
2021-04-25 19:15:16 +0400

    project-setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+      <Link to='/'>QUIZ</Link>
+    </div>
+  )
+}
+
+export default Header;
